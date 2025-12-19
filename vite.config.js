bc9c3e26e17387e635f42096a6faeb375723c3f4
Dmitrij <Programmer.IQ@Gmail.com>
2025-12-19 14:30:36 +0300

    Fix PWA orientation to portrait

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig({
         theme_color: '#000000',
         background_color: '#000000',
         display: 'standalone',
-        orientation: 'landscape',
+        orientation: 'portrait',
         icons: [
           {
             src: 'pwa-192x192.png',
